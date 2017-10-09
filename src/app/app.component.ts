9365e2b2a9b4440dc38152c24085e63993c0d42c
tzmanics <tzmanics@gmail.com>
2017-10-09 19:23:47 -0400

    add chart w filler data & default styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Visualizing Sensor Data';
+  private timestamps: number[]= [1507498189484, 1507498129444, 1507498159454,
+    1507498189484, 1507498219514];
+  private series: any[] = [{
+    name: 'light', 
+    data: [1.06, 1.46, 0.88, 1.27, 1.17]
+  }, { 
+    name: 'sound',
+    data: [1.76, 1.66, 1.56, 1.55, 1.46]
+  }];
 }
